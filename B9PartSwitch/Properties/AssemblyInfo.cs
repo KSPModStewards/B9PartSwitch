56e443b06a768b3652d062d79fb5966ac041c0fb
linuxgurugamer@gmail.com <linuxgurugamer@gmail.com>
2025-07-03 23:08:28 -0400

    Updated .gitignore

---
+++ B9PartSwitch/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("B9PartSwitch")]
+[assembly: AssemblyDescription("A part switching module for KSP")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("blowfish")]
+[assembly: AssemblyProduct("B9PartSwitch")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("011a01e9-0074-47d5-82f8-4259d3d927b7")]
+
+[assembly: AssemblyInformationalVersionAttribute("v2.7.0-dirty")]
+//[assembly: AssemblyVersion("2.7.0.0")]
+//[assembly: AssemblyFileVersion("2.7.0.0")]
+
+[assembly: KSPAssembly("B9PartSwitch", 2, 7, 0)]
